ba931e52065baf87e90fc9abae337bdf0a4e9ddf
hannahlee425 <hannahlee425@gmail.com>
2024-10-25 06:10:54 +0000

    사건 요약 이미지 버튼 수정

--- lib/screens/summary_screen.dart
+++ lib/screens/summary_screen.dart
@@ -99,91 +99,77 @@ class SummaryScreen extends StatelessWidget {
     return await model.generateContent([Content.text(refinedPrompt)]);
   }
 
+  // Body
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // AppBar
       appBar: AppBar(
         title: const Text("사건 요약"),
         centerTitle: true,
       ),
-
-      // Body
-      body: FutureBuilder<GenerateContentResponse>(
-        future: generateResponse(),
-        builder: (context, snapshot) {
-          // Gemini 응답을 기다릴 때
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-
-            // 응답 결과가 에러일 때
-          } else if (snapshot.hasError) {
-            return Center(
-              child: Text('Error: ${snapshot.error}'),
-            );
-
-            // 응답 결과가 null일 때
-          } else if (!snapshot.hasData || snapshot.data == null) {
-            return const Center(
-              child: Text('No data found.'),
-            );
-          } else {
-            // 응답 결과가 정상일 때
-            return SingleChildScrollView(
-              child: Center(
-                child: Container(
-                  margin: const EdgeInsets.all(5.0),
-                  padding: const EdgeInsets.all(10.0),
-                  constraints: BoxConstraints(
-                    maxWidth: MediaQuery.of(context).size.width * 0.90,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(16.0),
-                  ),
-                  child: Column(
-                    children: [
-                      MarkdownBody(
-                        selectable: true,
-                        data: snapshot.data!.text!,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            child: const Text('AI 이미지 1컷 생성'),
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                  context, '/imagen_1cut_generation',
-                                  arguments: snapshot.data!.text!);
-                            },
-                          ),
-                          const SizedBox(width: 15),
-                          ElevatedButton(
-                            child: const Text('AI 이미지 4컷 생성'),
-                            onPressed: () {
-                              Navigator.pushNamed(
-                                  context, '/imagen_4cut_generation',
-                                  arguments: snapshot.data!.text!);
-                            },
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                        child: const Text('Dalle Test'),
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/dalle_test');
-                        },
-                      ),
-                    ],
+      body: SafeArea(
+        child: FutureBuilder<GenerateContentResponse>(
+          future: generateResponse(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data == null) {
+              return const Center(child: Text('No data found.'));
+            } else {
+              return SingleChildScrollView(
+                child: Center(
+                  child: Container(
+                    margin: const EdgeInsets.all(5.0),
+                    padding: const EdgeInsets.all(10.0),
+                    constraints: BoxConstraints(
+                      maxWidth: MediaQuery.of(context).size.width * 0.90,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade300,
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        MarkdownBody(
+                          selectable: true,
+                          data: snapshot.data!.text!,
+                        ),
+                        const SizedBox(height: 20),
+                        ElevatedButton(
+                          child: const Text('Imagen3 AI 이미지 1컷 생성'),
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, '/imagen_1cut_generation',
+                                arguments: snapshot.data!.text!);
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          child: const Text('Imagen3 AI 이미지 4컷 생성'),
+                          onPressed: () {
+                            Navigator.pushNamed(
+                                context, '/imagen_4cut_generation',
+                                arguments: snapshot.data!.text!);
+                          },
+                        ),
+                        const SizedBox(height: 10),
+                        ElevatedButton(
+                          child: const Text('Dalle AI 이미지 1컷 생성'),
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/dalle_test');
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          }
-        },
+              );
+            }
+          },
+        ),
       ),
     );
   }
--- lib/utils/app_const.dart
+++ lib/utils/app_const.dart
@@ -1,9 +1,8 @@
 // Gemini API Key
-const apiKey = 'your api key';
 
 // Gemini Model
 const String geminiModelPro = "gemini-1.5-pro-latest";
-const String geminiModelFlash = "gemini-1.5-fl`ash-latest";
+const String geminiModelFlash = "gemini-1.5-flash-latest";
 // const String imagen3 = "imagen-3.0-generate-001";
 
 const String guidlines = """
